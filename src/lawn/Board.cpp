3a723ffc93e6e581fb1ee4eb2f0cb5ba408b3531
map220v <map220v300@gmail.com>
2025-04-29 16:22:30 +0300

    Fix arrow position in sunflower tutorial

--- src/lawn/Board.cpp
+++ src/lawn/Board.cpp
@@ -4821,7 +4821,7 @@ void Board::SetTutorialState(const TutorialState theTutorialState) {
 
     case TutorialState::TUTORIAL_LEVEL_2_PICK_UP_SUNFLOWER:
     case TutorialState::TUTORIAL_MORESUN_PICK_UP_SUNFLOWER:
-        TutorialArrowShow(mSeedBank->mX + mSeedBank->mSeedPackets[1].mX, mSeedBank->mX + mSeedBank->mSeedPackets[1].mX);
+        TutorialArrowShow(mSeedBank->mX + mSeedBank->mSeedPackets[1].mX, mSeedBank->mY + mSeedBank->mSeedPackets[1].mY);
         break;
 
     case TutorialState::TUTORIAL_LEVEL_2_PLANT_SUNFLOWER:
